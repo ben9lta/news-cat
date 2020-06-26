10af1b6bacddb63c1b6c31863d781f667c5b4fa5
Alexandr Bondar <ya.sasha.bondar30@gmail.com>
2020-06-27 01:53:46 +0300

    Migration. News: Создание таблицы News

---
+++ console/migrations/m200626_210416_create_news_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%news}}`.
+ */
+class m200626_210416_create_news_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%news}}', [
+            'id' => $this->primaryKey()->unsigned(),
+            'title' => $this->string(),
+            'body' => $this->text(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%news}}');
+    }
+}
