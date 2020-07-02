7ded295ebce3ce7c89388d8212a6c3f766686cf3
Alexandr Bondar <ya.sasha.bondar30@gmail.com>
2020-07-02 03:00:03 +0300

    Добавлена ссылка в панель (доступно всем авторизированным)

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -42,6 +42,7 @@ AppAsset::register($this);
         $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
         $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
     } else {
+        $menuItems[] = ['label' => 'Panel', 'url' => Yii::$app->params['panel']];
         $menuItems[] = '<li>'
             . Html::beginForm(['/site/logout'], 'post')
             . Html::submitButton(
