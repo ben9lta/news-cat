c8138ae8eab3b6aa7d99ba86609ba5db614a4c21
Alexandr Bondar <ya.sasha.bondar30@gmail.com>
2020-07-15 01:46:27 +0300

    public API

--- frontend/controllers/NewsController.php
+++ frontend/controllers/NewsController.php
@@ -3,9 +3,9 @@
 namespace frontend\controllers;
 
 use common\models\News;
-use common\models\RubricNews;
 use Yii;
 use yii\data\ActiveDataProvider;
+use yii\filters\Cors;
 use yii\rest\ActiveController;
 
 class NewsController extends ActiveController
@@ -14,19 +14,38 @@ class NewsController extends ActiveController
 
     public function behaviors()
     {
-        return [
-            [
-                'class' => \yii\filters\ContentNegotiator::className(),
-                'formats' => [
-                    'application/json' => \yii\web\Response::FORMAT_JSON,
+        $behaviors = parent::behaviors();
+
+        unset($behaviors['authenticator']);
+
+        $behaviors['corsFilter'] = [
+            'class' => Cors::className(),
+            'cors' => [
+                'Origin' => ['*'],
+                'Access-Control-Request-Method'  => ['GET'],
+                'Access-Control-Request-Headers' => ['*'],
+                'Access-Control-Max-Age'         => 3600,
+                'Access-Control-Expose-Headers'  => [
+                    'X-Pagination-Current-Page',
+                    'X-Pagination-Total-Count',
+                    'X-Pagination-Page-Count',
                 ],
+//                'Access-Control-Allow-Credentials' => true,
             ],
         ];
+
+//        $behaviors['authenticator'] = [
+//            'class' =>  HttpBearerAuth::className(),
+//            'except' => ['options','login'], // для гостей
+//        ];
+
+        return $behaviors;
     }
 
     public function actions()
     {
         $actions = parent::actions();
+
 
         unset($actions['delete'], $actions['create'], $actions['options'], $actions['update']);
         $actions['index']['prepareDataProvider'] = [$this, 'prepareDataProvider'];
@@ -34,15 +53,14 @@ class NewsController extends ActiveController
         return $actions;
     }
 
-    public function prepareDataProvider() {
+    public function prepareDataProvider()
+    {
         $id = Yii::$app->request->get('id');
+        $pageSize = Yii::$app->request->get('limit') ?? 5;
 
-        if(empty($id))
-        {
+        if (empty($id)) {
             $query = News::find()->orderBy(['id' => SORT_DESC]);
-        }
-        else
-        {
+        } else {
             $query = News::find()
                 ->select('news.id, title, body')
                 ->leftJoin('rubric_news', 'news.id = news_id')->andWhere(['rubric_id' => Yii::$app->request->get('id')])
@@ -52,7 +70,7 @@ class NewsController extends ActiveController
         return new ActiveDataProvider([
             'query' => $query,
             'pagination' => [
-                'pageSize' => 5,
+                'pageSize' => $pageSize,
             ],
         ]);
     }
